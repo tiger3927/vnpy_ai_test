2a0569a667dcb871bcc3c00a51339fb5d3136910
tiger-notebook <tiger3927@gmail.com>
2023-11-04 01:16:16 +0800

    1

---
+++ test_autots_1.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import datetime
+from autots import AutoTS
+from datetime import date, timedelta
+import yfinance as yf   # we can use this library to download some finance data
+d1 = date.today()
+end_date = d1.strftime("%Y-%m-%d")
+d2 = date.today()-timedelta(days=1000)
+start_date = d2.strftime("%Y-%m-%d")
+
+data = yf.download("BTC-USD"
+                  , start = start_date
+                  , end = end_date,
+                  progress = False)
+
+print(data.head())
+data["Date"] = data.index
+data.reset_index(drop=True, inplace= True)
+print(data.isnull().sum())
+print(data.head())
+# import plotly.graph_objects as go
+# figure = go.Figure(go.Candlestick(x = data["Date"]
+#                                  ,open = data["Open"]
+#                                  ,close = data["Close"]
+#                                  ,high = data["High"]
+#                                  ,low = data["Low"]))
+# figure.update_layout(title = " Bitcoin Price Analysis",
+#                     xaxis_rangeslider_visible = False)
+# figure.show()
+
+import seaborn as sns
+
+correlation = data.corr()
+correlation["Close"].sort_values(ascending = False)
+
+sns.heatmap(correlation, cmap= "coolwarm", annot = True)
+print(correlation)
+
+model = AutoTS(forecast_length = 30, ensemble = "simple", frequency = "infer")
+model = model.fit(data, date_col = "Date", value_col = "Close", id_col = None)
+prediction = model.predict()
+forecast = prediction.forecast
+print(forecast)
