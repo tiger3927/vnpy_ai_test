72b5a43ce36e313580c39895a6df1948e6f15fb7
tiger-notebook <tiger3927@gmail.com>
2023-11-04 12:45:57 +0800

    1

--- test_sklearn_1.py
+++ test_sklearn_1.py
@@ -1,7 +1,9 @@
 # encoding: UTF-8
 import warnings
-
-warnings.filterwarnings("ignore")
+import time
+import sys
+import os
+# warnings.filterwarnings("ignore")
 from pymongo import MongoClient, ASCENDING
 import pandas as pd
 import numpy as np
@@ -27,6 +29,46 @@ from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import SelectPercentile
 from sklearn.feature_selection import mutual_info_classif
 
+import random
+from typing import List
+from vnpy.trader.constant import Exchange, Interval
+from vnpy.trader.database import get_database
+from vnpy.trader.setting import SETTINGS
+from vnpy_ctastrategy import (
+    StopOrder,
+    TickData,
+    BarData,
+    TradeData,
+    OrderData,
+    BarGenerator,
+    ArrayManager,
+)
+from tiger_tools import load_json
+from numba import jit
+from sklearn.preprocessing import StandardScaler, minmax_scale
+from collections import deque
+import pandas
+import random
+from multiprocessing import Queue, Process
+
+config_path = ".vntrader"
+if not os.path.exists(config_path):
+    config_path = "../.vntrader"
+    if not os.path.exists(config_path):
+        config_path = "../../.vntrader"
+# print(config_path)
+# print(os.listdir(config_path))
+mysetting = load_json(config_path + "/vt_setting.json")
+
+SETTINGS["database.name"] = mysetting["database.name"]
+SETTINGS["database.database"] = mysetting["database.database"]
+SETTINGS["database.host"] = mysetting["database.host"]
+SETTINGS["database.port"] = mysetting["database.port"]
+SETTINGS["database.user"] = mysetting["database.user"]
+SETTINGS["database.password"] = mysetting["database.password"]
+
+database = get_database()
+
 
 class DataAnalyzerforSklearn(object):
     """
@@ -41,44 +83,149 @@ class DataAnalyzerforSklearn(object):
     8. RSI
     """
 
-    def __init__(self, exportpath="C:\\Project\\", datformat=['datetime', 'high', 'low', 'open', 'close', 'volume']):
-        self.mongohost = None
-        self.mongoport = None
-        self.db = None
-        self.collection = None
+    def __init__(self, name, symbol="ETHUSDT", exchange=Exchange.BINANCE, daily_end=None):
+        self.name = name
+        self.symbol = symbol
+        self.exchange = exchange
+        if daily_end:
+            self.daily_end = daily_end
+
         self.df = pd.DataFrame()
-        self.exportpath = exportpath
-        self.datformat = datformat
+        self.data_path = "./data/"
         self.startBar = 2
         self.endBar = 12
         self.step = 2
         self.pValue = 0.015
 
-    # -----------------------------------------导入数据-------------------------------------------------
-    def db2df(self, db, collection, start, end, mongohost="localhost", mongoport=27017, export2csv=False):
-        """读取MongoDB数据库行情记录，输出到Dataframe中"""
-        self.mongohost = mongohost
-        self.mongoport = mongoport
-        self.db = db
-        self.collection = collection
-        dbClient = MongoClient(self.mongohost, self.mongoport, connectTimeoutMS=500)
-        db = dbClient[self.db]
-        cursor = db[self.collection].find({'datetime': {'$gte': start, '$lt': end}}).sort("datetime", ASCENDING)
-        self.df = pd.DataFrame(list(cursor))
-        self.df = self.df[self.datformat]
-        self.df = self.df.reset_index(drop=True)
-        path = self.exportpath + self.collection + ".csv"
-        if export2csv == True:
-            self.df.to_csv(path, index=True, header=True)
-        return self.df
+        self.interval = Interval.MINUTE
+        self.start = datetime(2022, 1, 1)
+        self.end = datetime(2022, 1, 10)
+        self.daily_end = datetime(2023, 10, 10, 23, 59, 0).time()
 
-    def csv2df(self, csvpath, dataname="csv_data", export2csv=False):
+        self.bars_1m = []
+        self.bars_5m = []
+        self.bars_15m = []
+        self.bars_30m = []
+        self.bars_1h = []
+        self.bars_4h = []
+        self.bars_12h = []
+        self.bars_1d = []
+        self.bg1m = BarGenerator(self.on_bar, 1, self.on_1min_bar)  # 只需要对 bg1m 使用 update_bar 即可，其他自动产生
+        self.bg5m = BarGenerator(self.on_bar, 5, self.on_5min_bar)
+        self.bg15m = BarGenerator(self.on_bar, 15, self.on_15min_bar)
+        self.bg30m = BarGenerator(self.on_bar, 30, self.on_30min_bar)
+        self.bg1h = BarGenerator(self.on_bar, 1, self.on_1h_bar, interval=Interval.HOUR)
+        self.bg4h = BarGenerator(self.on_bar, 4, self.on_4h_bar, interval=Interval.HOUR)
+        self.bg12h = BarGenerator(self.on_bar, 12, self.on_12h_bar, interval=Interval.HOUR)
+        self.bg1d = BarGenerator(self.on_bar, 1, self.on_1d_bar, interval=Interval.DAILY, daily_end=self.daily_end)
+        self.df_1m: pandas.DataFrame = None
+        self.df_5m: pandas.DataFrame = None
+        self.df_15m: pandas.DataFrame = None
+        self.df_30m: pandas.DataFrame = None
+        self.df_1h: pandas.DataFrame = None
+        self.df_4h: pandas.DataFrame = None
+        self.df_12m: pandas.DataFrame = None
+        self.df_1d: pandas.DataFrame = None
+
+    def on_bar(self, bar: BarData):
+        pass
+
+    def on_1min_bar(self, bar: BarData):
+        self.bars_1m.append(bar)
+
+    def on_5min_bar(self, bar: BarData):
+        self.bars_5m.append(bar)
+
+    def on_15min_bar(self, bar: BarData):
+        self.bars_15m.append(bar)
+
+    def on_30min_bar(self, bar: BarData):
+        self.bars_30m.append(bar)
+
+    def on_1h_bar(self, bar: BarData):
+        self.bars_1h.append(bar)
+
+    def on_4h_bar(self, bar: BarData):
+        self.bars_4h.append(bar)
+
+    def on_12h_bar(self, bar: BarData):
+        self.bars_12h.append(bar)
+
+    def on_1d_bar(self, bar: BarData):
+        self.bars_1d.append(bar)
+
+    # -----------------------------------------导入数据-------------------------------------------------
+    def load(self, start, end):
+        self.start = start
+        self.end = end
+        t = time.time()
+        bars = database.load_bar_data(
+            symbol=self.symbol,
+            exchange=self.exchange,
+            interval=self.interval,
+            start=self.start,
+            end=self.end
+        )
+        print("调取数据库，耗时:", round(time.time() - t, 2), " 秒")
+        self.load_bars(bars)
+
+    def load_bars(self, bars: []):  # 用于从其他对象移植数据过来，不重复加载数据库
+        # 根据window，计算start 和 total
+        print("生成 5m 15m 30m 1h 4h 12h 1d ---- bars 数据 ----")
+        t = time.time()
+        for b in bars:
+            self.bg1m.update_bar(b)
+            self.bg5m.update_bar(b)
+            self.bg15m.update_bar(b)
+            self.bg30m.update_bar(b)
+            self.bg1h.update_bar(b)
+            self.bg4h.update_bar(b)
+            self.bg12h.update_bar(b)
+            self.bg1d.update_bar(b)
+        print("bars多周期生成耗时：", time.time() - t, " 秒")
+        t = time.time()
+
+        def bars_pandas(bars: [BarData]):
+            data = np.ndarray(shape=(len(bars), 6))
+            i = 0
+            ds = []
+            for b in bars:
+                data[i, 1] = b.open_price
+                data[i, 2] = b.high_price
+                data[i, 3] = b.low_price
+                data[i, 4] = b.close_price
+                data[i, 5] = b.volume
+                ds.append(b.datetime)
+                i += 1
+            dt_series = pandas.Series(ds)
+            print(len(ds))
+            df: pandas.DataFrame = pandas.DataFrame(columns=["open", "high", "low", "close", "volume"], index=dt_series)
+            df["open"] = data[:, 1]
+            df["high"] = data[:, 2]
+            df["low"] = data[:, 3]
+            df["close"] = data[:, 4]
+            df["volume"] = data[:, 5]
+            df.index.name = "datetime"
+            print(df.head())
+            return df
+
+        # pandas
+        self.df_1m = bars_pandas(self.bars_1m)
+        self.df_5m = bars_pandas(self.bars_5m)
+        self.df_15m = bars_pandas(self.bars_15m)
+        self.df_30m = bars_pandas(self.bars_30m)
+        self.df_1h = bars_pandas(self.bars_1h)
+        self.df_4h = bars_pandas(self.bars_4h)
+        self.df_12h = bars_pandas(self.bars_12h)
+        self.df_1d = bars_pandas(self.bars_1d)
+        print("pandas 生成耗时：", time.time() - t, " 秒")
+
+    def csv2df(self, csvname, dataname="csv_data", export2csv=False):
         """读取csv行情数据，输入到Dataframe中"""
-        csv_df = pd.read_csv(csvpath)
-        self.df = csv_df[self.datformat]
+        self.df = pd.read_csv(self.data_path + csvname)
         self.df["datetime"] = pd.to_datetime(self.df['datetime'])
         self.df = self.df.reset_index(drop=True)
-        path = self.exportpath + dataname + ".csv"
+        path = self.data_path + dataname + ".csv"
         if export2csv == True:
             self.df.to_csv(path, index=True, header=True)
         return self.df
@@ -117,12 +264,12 @@ class DataAnalyzerforSklearn(object):
                 openBarMin = 0
                 volumeBarmin = 0
         if export2csv == True:
-            dfbarmin.to_csv(self.exportpath + "bar" + str(barmins) + str(self.collection) + ".csv", index=True,
+            dfbarmin.to_csv(self.data_path + "bar" + str(barmins) + str(self.collection) + ".csv", index=True,
                             header=True)
         return dfbarmin
 
     # -----------------------------------------开始计算指标-------------------------------------------------
-    def dfcci(self, inputdf, n, export2csv=True):
+    def dfcci(self, inputdf, n):
         """调用talib方法计算CCI指标，写入到df并输出"""
         dfcci = inputdf
         dfcci["cci"] = None
@@ -132,11 +279,11 @@ class DataAnalyzerforSklearn(object):
             dfcci.loc[i, "cci"] = cci[-1]
         dfcci = dfcci.fillna(0)
         dfcci = dfcci.replace(np.inf, 0)
-        if export2csv == True:
-            dfcci.to_csv(self.exportpath + "dfcci" + str(self.collection) + ".csv", index=True, header=True)
+        # if export2csv == True:
+        #     dfcci.to_csv(self.data_path + "dfcci" + str(self.collection) + ".csv", index=True, header=True)
         return dfcci
 
-    def dfatr(self, inputdf, n, export2csv=True):
+    def dfatr(self, inputdf, n):
         """调用talib方法计算ATR指标，写入到df并输出"""
         dfatr = inputdf
         for i in range((n + 1), len(inputdf)):
@@ -145,11 +292,9 @@ class DataAnalyzerforSklearn(object):
             dfatr.loc[i, "atr"] = atr[-1]
         dfatr = dfatr.fillna(0)
         dfatr = dfatr.replace(np.inf, 0)
-        if export2csv == True:
-            dfatr.to_csv(self.exportpath + "dfatr" + str(self.collection) + ".csv", index=True, header=True)
         return dfatr
 
-    def dfrsi(self, inputdf, n, export2csv=True):
+    def dfrsi(self, inputdf, n):
         """调用talib方法计算ATR指标，写入到df并输出"""
         dfrsi = inputdf
         dfrsi["rsi"] = None
@@ -159,11 +304,9 @@ class DataAnalyzerforSklearn(object):
             dfrsi.loc[i, "rsi"] = rsi[-1]
         dfrsi = dfrsi.fillna(0)
         dfrsi = dfrsi.replace(np.inf, 0)
-        if export2csv == True:
-            dfrsi.to_csv(self.exportpath + "dfrsi" + str(self.collection) + ".csv", index=True, header=True)
         return dfrsi
 
-    def Percentage(self, inputdf, export2csv=True):
+    def Percentage(self, inputdf):
         """调用talib方法计算CCI指标，写入到df并输出"""
         dfPercentage = inputdf
         # dfPercentage["Percentage"] = None
@@ -180,12 +323,9 @@ class DataAnalyzerforSklearn(object):
             dfPercentage.loc[i, "Perentage"] = percentage
         dfPercentage = dfPercentage.fillna(0)
         dfPercentage = dfPercentage.replace(np.inf, 0)
-        if export2csv == True:
-            dfPercentage.to_csv(self.exportpath + "Percentage_" + str(self.collection) + ".csv", index=True,
-                                header=True)
         return dfPercentage
 
-    def dfMACD(self, inputdf, n, export2csv=False):
+    def dfMACD(self, inputdf, n):
         """调用talib方法计算MACD指标，写入到df并输出"""
         dfMACD = inputdf
         for i in range(n, len(inputdf)):
@@ -196,11 +336,9 @@ class DataAnalyzerforSklearn(object):
             dfMACD.loc[i, "hist"] = hist[-1]
         dfMACD = dfMACD.fillna(0)
         dfMACD = dfMACD.replace(np.inf, 0)
-        if export2csv == True:
-            dfMACD.to_csv(self.exportpath + "macd" + str(self.collection) + ".csv", index=True, header=True)
         return dfMACD
 
-    def dfSTD(self, inputdf, n, export2csv=False):
+    def dfSTD(self, inputdf, n):
         """调用talib方法计算MACD指标，写入到df并输出"""
         dfSTD = inputdf
         for i in range(n, len(inputdf)):
@@ -209,12 +347,10 @@ class DataAnalyzerforSklearn(object):
             dfSTD.loc[i, "std"] = std[-1]
         dfSTD = dfSTD.fillna(0)
         dfSTD = dfSTD.replace(np.inf, 0)
-        if export2csv == True:
-            dfSTD.to_csv(self.exportpath + "dfSTD" + str(self.collection) + ".csv", index=True, header=True)
         return dfSTD
 
     # -----------------------------------------加入趋势分类-------------------------------------------------
-    def addTrend(self, inputdf, trendsetp=6, export2csv=False):
+    def addTrend(self, inputdf, trendsetp=6):
         """以未来6个bar的斜率线性回归为判断分类是否正确"""
         dfTrend = inputdf
         for i in range(1, len(dfTrend) - trendsetp - 1):
@@ -228,8 +364,6 @@ class DataAnalyzerforSklearn(object):
                     dfTrend.loc[i, "tradeindictor"] = -1
         dfTrend = dfTrend.fillna(0)
         dfTrend = dfTrend.replace(np.inf, 0)
-        if export2csv == True:
-            dfTrend.to_csv(self.exportpath + "addTrend" + str(self.collection) + ".csv", index=True, header=True)
         return dfTrend
 
 
@@ -269,21 +403,22 @@ def GirdValuate(X_train, y_train):
 
 if __name__ == '__main__':
     # 读取数据
-    # exportpath = "C:\\Users\shui0\OneDrive\Documents\Project\\"
-    exportpath = "C:\Project\\"
-    DA = DataAnalyzerforSklearn(exportpath)
+    # data_path = "C:\\Users\shui0\OneDrive\Documents\Project\\"
+    data_path = "./data/"
+    DA = DataAnalyzerforSklearn("ethusdt_1m_1h_4h",symbol= "ETHUSDT", exchange=Exchange.BINANCE)
     # 数据库导入
-    start = datetime.strptime("20180501", '%Y%m%d')
-    end = datetime.strptime("20190501", '%Y%m%d')
-    df = DA.db2df(db="VnTrader_1Min_Db", collection="rb8888", start=start, end=end)
-    df5min = DA.df2Barmin(df, 5)
-    df5minAdd = DA.addTrend(df5min, export2csv=True)
-    df5minAdd = DA.dfMACD(df5minAdd, n=34, export2csv=True)
-    df5minAdd = DA.dfatr(df5minAdd, n=25, export2csv=True)
-    df5minAdd = DA.dfrsi(df5minAdd, n=35, export2csv=True)
-    df5minAdd = DA.dfcci(df5minAdd, n=30, export2csv=True)
-    df5minAdd = DA.dfSTD(df5minAdd, n=30, export2csv=True)
-    df5minAdd = DA.Percentage(df5minAdd, export2csv=True)
+    DA.load(datetime(2021, 1, 1), datetime(2021, 3, 1))    # df = DA.db2df(db="VnTrader_1Min_Db", collection="rb8888", start=start, end=end)
+    df5min = DA.df_5m
+    df5min.to_csv(data_path + "df5min" + ".csv", index=True, header=True)
+    df5minAdd = DA.addTrend(df5min)
+    df5minAdd = DA.dfMACD(df5minAdd, n=34)
+    df5minAdd = DA.dfatr(df5minAdd, n=25)
+    df5minAdd = DA.dfrsi(df5minAdd, n=35)
+    df5minAdd = DA.dfcci(df5minAdd, n=30)
+    df5minAdd = DA.dfSTD(df5minAdd, n=30)
+    df5minAdd = DA.Percentage(df5minAdd)
+    df5minAdd.to_csv(data_path + "df5minAdd" + ".csv", index=True, header=True)
+    print("生成数据表，并添加了特征...")
     # 划分测试验证。
     df_test = df5minAdd.loc[60:, :]  # 只从第60个开始分析，因为之前很多是空值
     y = np.array(df_test["tradeindictor"])  # 只保留结果趋势结果，转化为数组
@@ -328,7 +463,7 @@ if __name__ == '__main__':
     print(y_test)
     print(y_pred)
     dfresult = pd.DataFrame({'Actual': y_test, 'Predict': y_pred})
-    dfresult.to_csv(exportpath + "result" + ".csv", index=True, header=True)
+    dfresult.to_csv(data_path + "result" + ".csv", index=True, header=True)
 
     # 模型保存到本地
     joblib.dump(clf_selected, 'clf_selected.m')
