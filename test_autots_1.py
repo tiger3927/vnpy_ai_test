ca1d19cbae7366661004e9bd99ad3ea3af6d0385
tiger-notebook <tiger3927@gmail.com>
2023-11-06 03:11:06 +0800

    1

--- test_autots_1.py
+++ test_autots_1.py
@@ -3,21 +3,24 @@ import pandas as pd
 import datetime
 from autots import AutoTS
 from datetime import date, timedelta
-import yfinance as yf   # we can use this library to download some finance data
-d1 = date.today()
-end_date = d1.strftime("%Y-%m-%d")
-d2 = date.today()-timedelta(days=1000)
-start_date = d2.strftime("%Y-%m-%d")
+# import yfinance as yf   # we can use this library to download some finance data
+# d1 = date.today()
+# end_date = d1.strftime("%Y-%m-%d")
+# d2 = date.today()-timedelta(days=1000)
+# start_date = d2.strftime("%Y-%m-%d")
+#
+# data = yf.download("BTC-USD"
+#                   , start = start_date
+#                   , end = end_date,
+#                   progress = False)
+#
+# print(data.head())
+# data["Date"] = data.index
+# data.reset_index(drop=True, inplace= True)
+# print(data.isnull().sum())
 
-data = yf.download("BTC-USD"
-                  , start = start_date
-                  , end = end_date,
-                  progress = False)
-
-print(data.head())
-data["Date"] = data.index
-data.reset_index(drop=True, inplace= True)
-print(data.isnull().sum())
+data_path = "./data/"
+data = pd.read_csv(data_path+'ETHUSDT_5m_2020-01-01 00-00-00---2023-01-01 00-00-00.csv')
 print(data.head())
 # import plotly.graph_objects as go
 # figure = go.Figure(go.Candlestick(x = data["Date"]
@@ -29,13 +32,13 @@ print(data.head())
 #                     xaxis_rangeslider_visible = False)
 # figure.show()
 
-import seaborn as sns
-
-correlation = data.corr()
-correlation["Close"].sort_values(ascending = False)
-
-sns.heatmap(correlation, cmap= "coolwarm", annot = True)
-print(correlation)
+# import seaborn as sns
+#
+# correlation = data.corr()
+# correlation["Close"].sort_values(ascending = False)
+#
+# sns.heatmap(correlation, cmap= "coolwarm", annot = True)
+# print(correlation)
 
 model = AutoTS(forecast_length = 30, ensemble = "simple", frequency = "infer")
 model = model.fit(data, date_col = "Date", value_col = "Close", id_col = None)
